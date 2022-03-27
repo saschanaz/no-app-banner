3451cf5bf9623900f2cfd6a395416c15adfd678d
Kagami Sascha Rosylight <saschanaz@outlook.com>
2022-03-27 17:38:02 +0200

    fix(joongna): hide banner on other pages too

--- no-app-banner.user.js
+++ no-app-banner.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         no-app-banner
 // @namespace    http://github.com/saschanaz/no-app-banner
-// @version      0.1.1
+// @version      0.1.2
 // @description  Hide annoying banners that suggests installing apps
 // @author       Kagami Sascha Rosylight
-// @match        https://m.joongna.com/home
+// @match        https://m.joongna.com/*
 // @match        https://www1.president.go.kr/petitions*
 // @match        https://www.reddit.com/*
 // @grant        none
-// @updateURL    https://github.com/saschanaz/no-app-banner/raw/main/no-app-banner.user.js
 // ==/UserScript==
 
 (function() {
@@ -33,7 +32,7 @@
     if (location.host === "m.joongna.com") {
         // Removing portal breaks the page
         document.getElementById("portal").setAttribute("hidden", "");
-        insertStyle("footer + div { display: none }");
+        insertStyle("#root button:not([class]):not([to]) { display: none }");
         document.body.style.overflow = "initial";
         createCSSChangeObserver(document.body, () => {
             document.body.style.overflow = "initial";
